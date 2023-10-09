9c5824b9622955cc7efb0946afe9f917758f16db
lucasdxvid <athl12311@gmail.com>
2023-10-09 07:00:09 -0300

    general

---
+++ src/routes/views.router.js
@@ -0,0 +1,29 @@
+import { Router } from "express";
+import ProductManager from "../managers/ProductManagers.js";
+import { __dirname } from "../utils.js";
+import path from "node:path";
+
+const productsFilePath = path.join(__dirname, "./files/products.json");
+const productManager = new ProductManager(productsFilePath);
+
+const router = Router();
+
+router.get("/realtimeproducts", (req, res) => {
+  res.render("realTimeProducts", {
+    style: "index.css",
+  });
+});
+
+router.get("/", async (req, res) => {
+  try {
+    const products = await productManager.getProducts();
+    res.render("home", {
+      style: "index.css",
+      products,
+    });
+  } catch (error) {
+    res.status(500).send("Error al cargar los productos");
+  }
+});
+
+export default router;
